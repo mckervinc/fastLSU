27feca65c2c6343734f98a76ccd425ac37e30ec3
mckervinc <mckervinc@live.com>
2016-04-19 04:37:48 -0400

    Glorified tuple class

--- src/PValues.java
+++ src/PValues.java
@@ -11,16 +11,6 @@ public class PValues {
     prime = 0.0;
   }
 
-  public PValues concatenate(PValues b) {
-    if (this.array.length == 0) return b;
-    if (b.array.length == 0) return this;
-    PValues c = new PValues(this.array.length + b.array.length);
-    System.arraycopy(this.array, 0, c.array, 0, this.array.length);
-    System.arraycopy(b.array, 0, c.array, this.array.length, b.array.length);
-    this.prime += b.prime;
-    return c;
-  }
-
   public int size() {
     return array.length;
   }
